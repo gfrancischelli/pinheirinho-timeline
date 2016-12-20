308b2da566b0d2ed0eee627c02bd78fb52b62f6f
Giovanni Francischelli <gifrancischelli@gmail.com>
2016-12-20 20:56:11 -0200

    Config wepack

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    publicPath: '',
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react'],
+        },
+      }
+    ]
+  }
+}
